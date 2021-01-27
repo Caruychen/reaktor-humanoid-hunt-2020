aa25510cd43033300f0a74318edfa1a7798b5486
Caruychen <caruychen@gmail.com>
2021-01-27 13:22:39 +0200

    start creating classes

---
+++ puzzle_3/graph.py
@@ -0,0 +1,6 @@
+class Graph:
+    def __init__(self, adjacencyList=[], pathMatrix=[], end={}):
+        self.adjacencyList = adjacencyList
+        self.pathMatrix = pathMatrix
+        self.end = end
+
---
+++ puzzle_3/modules/neuralstrand.py
@@ -0,0 +1,31 @@
+class NeuralStrand:
+    def __init__(self, inputLine=''):
+        try:
+            self.setStrand(inputLine)
+        except:
+            self.start = None
+            self.path = None
+
+    def setStrand(self, inputLine):
+        splitLine = inputLine.strip().split(' ')
+        self.setStart(splitLine)
+        self.setPath(splitLine)
+
+    def setStart(self, splitLine):
+        self.start = [int(coordinate) for coordinate in splitLine[0].split(',')]
+    
+    def setPath(self, splitLine):
+        try:
+            self.path = splitLine[1].split(',')
+        except:
+            self.path = ''
+
+if __name__ == '__main__':
+    f = open("puzzle3.txt", "r")
+    strands = []
+    for line in f:
+        strand = NeuralStrand()
+        strand.setStrand(line)
+        strands.append(strand.start)
+    print(strands)
+    
